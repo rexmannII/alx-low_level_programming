c73cb7f4f89ddb33609a59bc3174b08bede8b0e9
rexmannII <onyegbulerexejike@gmail.com>
2024-02-05 05:08:32 -0800

    a function that adds a new node at the end of a listint_t list.

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,8 +2,8 @@
 /**
  * print_listint - print the int data in a singly linked list
  *
- * @h; the head pointer to the list
- * Return The number of the nodes in the list
+ * @h: the head pointer to the list
+ * Return: The number of the nodes in the list
  */
 size_t print_listint(const listint_t *h)
 {
